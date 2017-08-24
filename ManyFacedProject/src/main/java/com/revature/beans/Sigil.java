1b4e775681875526399485d6af274d7678c743f3
Evan Molinelli <emolinelli85@gmail.com>
2017-08-24 13:00:43 -0400

    weapons and sigil beans

---
+++ ManyFacedProject/src/main/java/com/revature/beans/Sigil.java
@@ -0,0 +1,55 @@
+package com.revature.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity // mapped to Hibernate
+@Table(name = "SIGIL")
+public class Sigil {
+
+	@Id // this field is the primary key identifier
+	@Column(name = "SIGIL_ID")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sigilSeq")
+	@SequenceGenerator(name = "sigilSeq", sequenceName = "SIGIL_BEAN_SEQ")
+	private int sigilId;
+	@Column(name = "SIGIL_NAME")
+	private String sigilName;
+
+	public Sigil() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Sigil(int sigilId, String sigilName) {
+		super();
+		this.sigilId = sigilId;
+		this.sigilName = sigilName;
+	}
+
+	public int getSigilId() {
+		return sigilId;
+	}
+
+	public void setSigilId(int sigilId) {
+		this.sigilId = sigilId;
+	}
+
+	public String getSigilName() {
+		return sigilName;
+	}
+
+	public void setSigilName(String sigilName) {
+		this.sigilName = sigilName;
+	}
+
+	@Override
+	public String toString() {
+		return "Sigil [sigilId=" + sigilId + ", sigilName=" + sigilName + "]";
+	}
+
+}
---
+++ ManyFacedProject/src/main/java/com/revature/beans/Weapons.java
@@ -0,0 +1,55 @@
+package com.revature.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "WEAPONS")
+public class Weapons {
+
+	@Id // this field is the primary key identifier
+	@Column(name = "WEAPONS_ID")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "weaponsSeq")
+	@SequenceGenerator(name = "weaponsSeq", sequenceName = "WEAPONS_BEAN_SEQ")
+	private int weaponId;
+	@Column(name = "WEAPON_NAME")
+	private String weaponName;
+
+	public Weapons() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Weapons(int weaponId, String weaponName) {
+		super();
+		this.weaponId = weaponId;
+		this.weaponName = weaponName;
+	}
+
+	public int getWeaponId() {
+		return weaponId;
+	}
+
+	public void setWeaponId(int weaponId) {
+		this.weaponId = weaponId;
+	}
+
+	public String getWeaponName() {
+		return weaponName;
+	}
+
+	public void setWeaponName(String weaponName) {
+		this.weaponName = weaponName;
+	}
+
+	@Override
+	public String toString() {
+		return "Weapons [weaponId=" + weaponId + ", weaponName=" + weaponName + "]";
+	}
+
+}
