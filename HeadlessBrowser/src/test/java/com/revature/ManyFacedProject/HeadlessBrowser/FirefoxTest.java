08d9482171012f1da36d8ec4e107074f0c66c631
Evan Molinelli <emolinelli85@gmail.com>
2017-09-15 16:52:19 -0400

    headless browser testing

--- HeadlessBrowser/src/test/java/com/revature/ManyFacedProject/HeadlessBrowser/FirefoxTest.java
+++ HeadlessBrowser/src/test/java/com/revature/ManyFacedProject/HeadlessBrowser/FirefoxTest.java
@@ -2,15 +2,18 @@ package com.revature.ManyFacedProject.HeadlessBrowser;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 
 public class FirefoxTest {
 
 	   @Test
 	   public void openBlazeMeterTest() {
-	       FirefoxDriver driver = new FirefoxDriver();
-	       driver.get("http://blazemeter.com");
+	       WebDriver driver = new HtmlUnitDriver(BrowserVersion.INTERNET_EXPLORER, true);
+	       driver.get("http://http://52.14.9.154:8080/index.html");
 	       WebElement title = driver.findElement(By.xpath("//title"));
 	       System.out.println("********************************************************");
 	       System.out.println("*" + title.getAttribute("text") + "*");
